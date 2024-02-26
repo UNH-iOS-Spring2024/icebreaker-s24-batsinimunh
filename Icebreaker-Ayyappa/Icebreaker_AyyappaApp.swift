1874172f8bc4f713d1934d27fb5c6fd68a750923
Ayyappa Tata <atata3@unh.newhaven.edu>
2024-02-26 12:47:54 -0500

    Initial Commit

---
+++ Icebreaker-Ayyappa/Icebreaker_AyyappaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Icebreaker_AyyappaApp.swift
+//  Icebreaker-Ayyappa
+//
+//  Created by Ayyappa Tata on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Icebreaker_AyyappaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
